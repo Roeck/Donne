39e8625cddba0a45d55cfd9d38ece357ba530819
rons9n <rons9n@gmail.com>
2022-04-20 19:57:24 -0400

    LandingPage component and styles update

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import  Logo  from '../components/Logo';
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            Job <span>Tracker</span>
+          </h1>
+          <p>
+            App description
+          </p>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  )
+}
+
+export default Landing
