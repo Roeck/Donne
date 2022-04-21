efcf373d4e88f1f146abdf4560786c7e719c09c6
rons9n <rons9n@gmail.com>
2022-04-21 16:18:31 -0400

    Error page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import img from '../assets/images/not-found.svg'
+import Wrapper from '../assets/wrappers/ErrorPage'
 
 const Error = () => {
-  return <h1>Error</h1>
+  return (
+    <Wrapper className='full-page'>
+      <div>
+        <img src={img} alt='not found' />
+        <h3>Page NOT found!</h3>
+        <p>Invalid Request</p>
+        <Link to='/'>Back Home</Link>
+      </div>
+    </Wrapper>
+  )
 }
 
 export default Error
