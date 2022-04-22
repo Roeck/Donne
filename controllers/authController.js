c4bad4ddd2a650b22d527eec9ac171140675c376
rons9n <rons9n@gmail.com>
2022-04-22 03:37:23 -0400

    BadRequestError and NotFoundError classes

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,11 +8,25 @@ class CustomAPIError extends Error {
     }
 }
 
+class BadRequestError extends CustomAPIError {
+    constructor(message) {
+        super(message)
+        this.statusCode = StatusCodes.BAD_REQUEST
+    }
+}
+
+class NotFoundError extends CustomAPIError {
+    constructor(message) {
+        super(message)
+        this.statusCode = StatusCodes.NOT_FOUND
+    }
+}
+
 const register = async (req, res) => {
     const { name, email, password } = req.body
 
     if (!name || !email || !password) {
-        throw new CustomAPIError ('Please, provide all values')
+        throw new BadRequestError ('Please, provide all values')
     }
 
     const user = await User.create({ name, email, password })
