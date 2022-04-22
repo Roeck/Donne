2ab403f6f6af7f235a683ad7cc2bf8c4a6e69165
rons9n <rons9n@gmail.com>
2022-04-22 03:25:24 -0400

    Empty values validation

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,11 @@
 import { StatusCodes } from "http-status-codes"
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    console.log(err)
+    console.log(err.message)
+
     const defaultError = {
         statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-        msg: "Something went wrong. Try again"
+        msg: err.message || "Something went wrong. Try again"
     }
     if (err.name === 'ValidationError'){
         defaultError.statusCode = StatusCodes.BAD_REQUEST
