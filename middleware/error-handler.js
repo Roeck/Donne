ea4018a04e6d82d8d658b8a343af51001fd51f57
rons9n <rons9n@gmail.com>
2022-04-22 02:42:38 -0400

    StatusCodes async errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,11 @@
+import { StatusCodes } from "http-status-codes"
+
 const errorHandlerMiddleware = (err, req, res, next) => {
     console.log(err)
+    const defaultError = {
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: "Something went wrong. Try again"
+    }
     res.status(500).json({ msg: 'There was an error'})
 }
 
