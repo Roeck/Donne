433daffd98ef2c9a7f4e4065954294db6e6ed741
rons9n <rons9n@gmail.com>
2022-04-25 18:09:46 -0400

    Redirect to dashboard after register

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Logo, FormRow, Alert } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
 import { useAppContext } from '../contexts/appContext'
+import { useNavigate } from 'react-router-dom'
 
 const initialState = {
     name: '',
@@ -11,9 +12,9 @@ const initialState = {
 }
 
 const Register = () => {
+  const navigate = useNavigate()
   const [values, setValues] = useState(initialState)
-
-  const { isLoading, showAlert, displayAlert, registerUser }  = useAppContext()
+  const { user, isLoading, showAlert, displayAlert, registerUser }  = useAppContext()
 
   const toggleMember = () => {
       setValues({...values, isMember: !values.isMember})
@@ -38,6 +39,14 @@ const Register = () => {
           registerUser(currentUser)
       }
   }
+
+  useEffect(() => {
+      if (user) {
+          setTimeout(() => {
+              navigate('/')
+          }, 3000)
+      }
+  }, [user, navigate])
 
   return (
     <Wrapper className='full-page'>
