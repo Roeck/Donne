c0e7ebe61ceb145c214e6e6fbf64a123ca03a731
rons9n <rons9n@gmail.com>
2022-04-25 20:47:31 -0400

    Dashboard pages structured routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<div>Dashboard</div>} />
+        <Route path='/'>
+          <Route path="add-job" element={<AddJob />}/>
+          <Route path="all-jobs" element={<AllJobs />}/>
+          <Route path="profile" element={<Profile />}/>
+          <Route path="stats" element={<Stats />}/>
+        </Route>
         <Route path='/register' element={<Register />} />
         <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
