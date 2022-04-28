d63a97be969dbcac020ee7384f62fe3ce489f419
rons9n <rons9n@gmail.com>
2022-04-28 14:19:39 -0400

    BigSidebar complete

--- client/src/assets/wrappers/BigSidebar.js
+++ client/src/assets/wrappers/BigSidebar.js
@@ -6,13 +6,14 @@ const Wrapper = styled.aside`
     display: block;
     box-shadow: 1px 0px 0px 0px rgba(0, 0, 0, 0.1);
     .sidebar-container {
+      background: #0f0e136e;
+      backdrop-filter: blur(2px);
       min-height: 100vh;
       height: 100%;
       width: 250px;
       margin-left: -250px;
       transition: var(--transition);
-      background: #0f0e136e;
-      backdrop-filter: blur(2px);
+      text-decoration: none;
     }
     .content {
       position: sticky;
@@ -26,6 +27,42 @@ const Wrapper = styled.aside`
       display: flex;
       align-items: center;
       padding-left: 2.5rem;
+    }
+    .nav-links {
+      padding-top: 2rem;
+      display: flex;
+      flex-direction: column;
+    }
+    .nav-link {
+      display: flex;
+      align-items: center;
+      color: var(--grey-200);
+      padding: 1rem 0;
+      padding-left: 2.5rem;
+      text-transform: capitalize;
+      transition: var(--transition);
+      text-decoration: none;
+    }
+    .nav-link:hover {
+      background: #0f0e136e;
+      padding-left: 3rem;
+      color: var(--white);
+    }
+    .nav-link:hover .icon {
+      color: var(--primary-500);
+    }
+    .icon {
+      font-size: 1.5rem;
+      margin-right: 1rem;
+      display: grid;
+      place-items: center;
+      transition: var(--transition);
+    }
+    .active {
+      color: var(--white);
+    }
+    .active .icon {
+      color: var(--primary-500);
     }
   }
 `
