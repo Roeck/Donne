01aa102e4d37257ae3c48b2c673fd89243c8071c
rons9n <rons9n@gmail.com>
2022-04-29 19:59:15 -0400

    Job state values

--- client/src/contexts/appContext.js
+++ client/src/contexts/appContext.js
@@ -32,7 +32,16 @@ const initialState = {
     token: token, 
     userLocation: userLocation || '',
     jobLocation: userLocation || '',
-    showSidebar: false
+    showSidebar: false,
+    isEditing: false,
+    editJobId: '',
+    position: '',
+    company: '',
+    jobLocation: userLocation || '',
+    jobTypeOptions: ['full-time', 'part-time', 'remote', 'internship'],
+    jobType: 'full-time',
+    statusOptions: ['interview', 'declined', 'pending'],
+    status: 'pending'
 }
 
 const AppContext = React.createContext()
