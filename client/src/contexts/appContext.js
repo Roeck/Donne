6653788cc66d01cb6f4fc6e7d6d3ead5ebeb25a2
rons9n <rons9n@gmail.com>
2022-04-30 19:31:37 -0400

    Get all jobs request

--- client/src/contexts/appContext.js
+++ client/src/contexts/appContext.js
@@ -19,7 +19,9 @@ import {
     CLEAR_VALUES,
     CREATE_JOB_BEGIN,
     CREATE_JOB_SUCCESS,
-    CREATE_JOB_ERROR
+    CREATE_JOB_ERROR,
+    GET_JOBS_BEGIN,
+    GET_JOBS_SUCCESS
  } from './actions'
 
 import reducer from './reducer'
@@ -209,6 +211,27 @@ const AppProvider = ({children}) => {
               type: CREATE_JOB_ERROR, 
               payload: { msg: error.response.data.msg } 
           })
+        }
+        clearAlert()
+    }
+
+    const getJobs = async () => {
+        let url = `/jobs`
+
+        dispatch({ type: GET_JOBS_BEGIN })
+        try {
+            const { data } = await authFetch(url)
+            const { jobs, totalJobs, numOfPages } = data
+            dispatch({
+                type: GET_JOBS_SUCCESS,
+                payload: {
+                    jobs,
+                    totalJobs,
+                    numOfPages
+                },
+            })
+        } catch (error) {
+            console.log(error.response)
         }
         clearAlert()
     }
--- client/src/contexts/reducer.js
+++ client/src/contexts/reducer.js
@@ -16,10 +16,12 @@ import {
     CLEAR_VALUES,
     CREATE_JOB_BEGIN,
     CREATE_JOB_SUCCESS,
-    CREATE_JOB_ERROR
+    CREATE_JOB_ERROR,
+    GET_JOBS_BEGIN,
+    GET_JOBS_SUCCESS
  } from "./actions"
 
- import { initialState } from "./appContext"
+import { initialState } from "./appContext"
 
 const reducer = (state, action) => {
     if (action.type === DISPLAY_ALERT) {
@@ -180,8 +182,16 @@ const reducer = (state, action) => {
             alertText: action.payload.msg
         }
     }
-
-
+    if (action.type === GET_JOBS_BEGIN) {
+        return { ...state, isLoading: true, showAlert: false }
+    }
+    if (action.type === GET_JOBS_SUCCESS) {
+        return { ...state, isLoading: false,
+             jobs: action.payload.jobs,
+             totalJobs: action.payload.totalJobs,
+             numOfPages: action.payload.numOfPages
+        }
+    }
     throw new Error(`no such action : ${action.type}`)
 }
 
