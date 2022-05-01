75e8331c0bd7ea01d2aa08ef31729215fbe464e3
rons9n <rons9n@gmail.com>
2022-05-01 17:43:48 -0400

    Delete job controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -50,8 +50,22 @@ const updateJob = async (req, res) => {
 // DELETE JOB
 
 const deleteJob = async (req, res) => {
-    res.send('delete job')
+    const { id: jobId } = req.params
+  
+    const job = await Job.findOne({ _id: jobId })
+  
+    if (!job) {
+      throw new NotFoundError(`No job with id :${jobId}`)
+    }
+  
+    checkPermissions(req.user, job.createdBy)
+  
+    await job.remove()
+  
+    res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' })
 }
+
+// SHOW STATS
 
 const showStats = async (req, res) => {
     res.send('show stats')
