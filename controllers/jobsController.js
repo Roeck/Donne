bdb3ed0ca06dccd4ce953e84329282bed4b52f50
rons9n <rons9n@gmail.com>
2022-05-02 18:30:29 -0400

    Aggregation based on month and year

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -5,66 +5,66 @@ import checkPermissions from '../utils/checkPermissions.js'
 import mongoose from 'mongoose'
 
 const createJob = async (req, res) => {
-    const { position, company } = req.body
+  const { position, company } = req.body
 
-    if(!position || !company) {
-        throw new BadRequestError('Please, provide all values')
-    }
-    req.body.createdBy = req.user.userId
-    const job = await Job.create(req.body)
-    res.status(StatusCodes.CREATED).json({ job })
+  if (!position || !company) {
+    throw new BadRequestError('Please, provide all values')
+  }
+  req.body.createdBy = req.user.userId
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json({ job })
 }
 
 const getAllJobs = async (req, res) => {
-    const jobs = await Job.find({ createdBy: req.user.userId })
-    res 
-        .status(StatusCodes.OK)
-        .json({ jobs, totalJobs: jobs.length, numOfPages: 1})
+  const jobs = await Job.find({ createdBy: req.user.userId })
+  res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
 }
 
 // UPDATE JOB
 
 const updateJob = async (req, res) => {
-    const { id: jobId } = req.params
-    const { company, position } = req.body
-  
-    if (!position || !company) {
-      throw new BadRequestError('Please provide all values')
-    }
-    const job = await Job.findOne({ _id: jobId })
-  
-    if (!job) {
-      throw new NotFoundError(`No job with id :${jobId}`)
-    }
-    
-    // check permissions
-  
-    checkPermissions(req.user, job.createdBy)
-  
-    const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
-      new: true,
-      runValidators: true,
-    })
-  
-    res.status(StatusCodes.OK).json({ updatedJob })
+  const { id: jobId } = req.params
+  const { company, position } = req.body
+
+  if (!position || !company) {
+    throw new BadRequestError('Please provide all values')
+  }
+  const job = await Job.findOne({ _id: jobId })
+
+  if (!job) {
+    throw new NotFoundError(`No job with id :${jobId}`)
+  }
+
+  // check permissions
+
+  checkPermissions(req.user, job.createdBy)
+
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
+  res.status(StatusCodes.OK).json({ updatedJob })
 }
 
 // DELETE JOB
 
 const deleteJob = async (req, res) => {
-    const { id: jobId } = req.params
-  
-    const job = await Job.findOne({ _id: jobId })
-  
-    if (!job) {
-      throw new NotFoundError(`No job with id :${jobId}`)
-    }
-  
-    checkPermissions(req.user, job.createdBy)
-  
-    await job.remove()
-  
-    res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' })
+  const { id: jobId } = req.params
+
+  const job = await Job.findOne({ _id: jobId })
+
+  if (!job) {
+    throw new NotFoundError(`No job with id :${jobId}`)
+  }
+
+  checkPermissions(req.user, job.createdBy)
+
+  await job.remove()
+
+  res.status(StatusCodes.OK).json({ msg: 'Success! Job removed' })
 }
 
 // SHOW STATS
@@ -86,7 +86,17 @@ const showStats = async (req, res) => {
     declined: stats.declined || 0,
   }
 
-  let monthlyApplications = []
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { '_id.year': -1, '_id.month': -1 } },
+    { $limit: 6 },
+  ])
 
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications })
 
