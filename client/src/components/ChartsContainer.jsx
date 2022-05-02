0d46a78cb0e09323d5b3fab41792695094b28958
rons9n <rons9n@gmail.com>
2022-05-02 18:58:53 -0400

    ChartContainer wrapper

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import BarChart from './BarChart'
 import AreaChart from './AreaChart'
 import { useAppContext } from '../contexts/appContext'
+import Wrapper from '../assets/wrappers/ChartsContainer'
 
 const ChartsContainer = () => {
+  const [barChart, setBarChart] = useState(true)
   const { monthlyApplications: data } = useAppContext()
   return (
-    <>
+    <Wrapper>
       <h4>Monthly Applications</h4>
-      <BarChart data={data} />
-      <AreaChart data={data} />
-    </>
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
   )
 }
 
