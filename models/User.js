74bd2cf03315a62005542bcf82b6b66ef04f5b2d
rons9n <rons9n@gmail.com>
2023-01-04 11:24:42 -0500

    Heroku removal

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose'
 import validator from 'validator'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -30,25 +29,26 @@ const UserSchema = new mongoose.Schema({
     type: String,
     trim: true,
     maxlength: 20,
-    default: 'Last name',
+    default: 'lastName',
   },
   location: {
     type: String,
     trim: true,
     maxlength: 20,
-    default: 'My city',
+    default: 'my city',
   },
 })
 
-UserSchema.pre('save', async function() {
-  if(!this.isModified('password')) return
+UserSchema.pre('save', async function () {
+  // console.log(this.modifiedPaths())
+  if (!this.isModified('password')) return
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
 })
 
 UserSchema.methods.createJWT = function () {
   return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
-     expiresIn: process.env.JWT_LIFETIME,
+    expiresIn: process.env.JWT_LIFETIME,
   })
 }
 
